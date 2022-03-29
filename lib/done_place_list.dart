a5683910236f9a713802947badd2de1a7b36f056
Layhome <78995627+layhome12@users.noreply.github.com>
2022-03-29 19:27:47 +0700

    State Management

---
+++ lib/done_place_list.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:layout_basic/models/tourism_model.dart';
+import 'package:layout_basic/provider/done_tourism_provider.dart';
+import 'package:provider/provider.dart';
+
+class DonePlaceList extends StatelessWidget {
+  const DonePlaceList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<TourismPlace> doneTourismPlaceList =
+        Provider.of<DoneTourismProvider>(context, listen: false)
+            .doneTourismPlaceList;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wisata Dikunjungi'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final TourismPlace place = doneTourismPlaceList[index];
+          return Card(
+            color: Colors.white54,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.asset(place.imageAssets),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            place.name,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 0, top: 4),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              place.location,
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const Icon(Icons.done_outline)
+              ],
+            ),
+          );
+        },
+        itemCount: doneTourismPlaceList.length,
+      ),
+    );
+  }
+}
